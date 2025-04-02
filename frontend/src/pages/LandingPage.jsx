a0ed7d9a9cdc5d9eaa85914c39a8cb0ff38d1ac1
GunvantPatwardhan <gautamphadnis2019@gmail.com>
2025-04-03 03:19:26 +0530

    Adjust footer copyright text alignment with left padding

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -644,7 +644,7 @@ const LandingPage = () => {
               >
                 {isWideFooter && (
                   /* Left Section when wide */
-                  <Box alignSelf="flex-start" paddingLeft="20px">
+                  <Box alignSelf="flex-start">
                     <Text mt="469px" fontSize="16px" lineHeight="20px">
                       All rights reserved © 2024 Vellonex
                     </Text>
