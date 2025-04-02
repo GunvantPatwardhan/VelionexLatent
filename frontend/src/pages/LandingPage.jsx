cd0e1627440afcf772160d08f6efa03c444f4718
GunvantPatwardhan <gautamphadnis2019@gmail.com>
2025-04-03 03:01:11 +0530

    Fix footer text positioning to bottom left corner

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -644,8 +644,8 @@ const LandingPage = () => {
               >
                 {isWideFooter && (
                   /* Left Section when wide */
-                  <Box position="absolute" bottom="10px" left="0">
-                    <Text fontSize="16px" lineHeight="20px">
+                  <Box>
+                    <Text mt="550px" fontSize="16px" lineHeight="20px">
                       All rights reserved © 2024 Vellonex
                     </Text>
                   </Box>
